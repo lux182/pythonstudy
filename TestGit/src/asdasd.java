44ae73419e97c3da101f81c6ea902dfff5780574
lux182 <562182429@qq.com>
2013-09-05 11:25:20 +0800

    dasad

---
+++ TestGit/src/asdasd.java
@@ -0,0 +1,4 @@
+
+public class asdasd {
+String s = "说汉语";//韩语汉语
+}
--- TestGit/src/chineseText.java
+++
@@ -1,9 +0,0 @@
-
-public class chineseText {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("我晕,真坑爹!");//中文好
-	}
-
-}
