e226c600e340347eeb5eb3d7f448beef5be02e8a
lux182 <562182429@qq.com>
2013-09-05 11:21:46 +0800

    ss

--- TestGit/src/chineseText.java
+++ TestGit/src/chineseText.java
@@ -3,7 +3,7 @@ public class chineseText {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("我晕,真坑爹!");//中文
+		System.out.println("我晕,真坑爹!");//中文好
 	}
 
 }
